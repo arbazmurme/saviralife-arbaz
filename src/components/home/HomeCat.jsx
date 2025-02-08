079285494cc4697076401723b6a844ba0e2ba9a7
arbazmurme <arbazmurme@gmail.com>
2025-02-08 12:57:22 +0530

    home cart same thing is fix

--- src/components/home/HomeCat.jsx
+++ src/components/home/HomeCat.jsx
@@ -1,6 +1,12 @@
 "use client";
 import React from "react";
-import { Navigation, Pagination, Mousewheel, Keyboard, Autoplay } from "swiper/modules";
+import {
+  Navigation,
+  Pagination,
+  Mousewheel,
+  Keyboard,
+  Autoplay,
+} from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Link from "next/link";
 
@@ -11,20 +17,70 @@ import "swiper/css/scrollbar";
 
 const HomeCat = () => {
   const categories = [
-    { title: "Tea Bags", imgSrc: "https://cdn.shopify.com/s/files/1/0905/2012/files/teapoz_image_45.png", alt: "Tea Bags" },
-    { title: "Maté", imgSrc: "https://cdn.shopify.com/s/files/1/0905/2012/files/Mate_9.png?v=1685006070", alt: "Maté" },
-    { title: "Matcha", imgSrc: "https://cdn.shopify.com/s/files/1/0905/2012/files/teapoz_image-54.png?v=1684921221", alt: "Matcha" },
-    { title: "Oolong", imgSrc: "https://cdn.shopify.com/s/files/1/0905/2012/files/teapoz_image_49.png", alt: "Oolong" },
-    { title: "Loose Tea", imgSrc: "https://cdn.shopify.com/s/files/1/0905/2012/files/teapoz_image_46.png", alt: "Loose Tea" },
-    { title: "Green Tea", imgSrc: "https://cdn.shopify.com/s/files/1/0905/2012/files/teapoz_image_47.png", alt: "Green Tea" },
-    { title: "Gift Sets", imgSrc: "https://cdn.shopify.com/s/files/1/0905/2012/files/teapoz_image_48.png", alt: "Gift Sets" },
-    { title: "Teaware", imgSrc: "https://cdn.shopify.com/s/files/1/0905/2012/files/teaware.jpg?v=1685006326", alt: "Teaware" },
-    { title: "Black Tea", imgSrc: "https://cdn.shopify.com/s/files/1/0905/2012/files/teapoz_image_44.png", alt: "Black Tea" },
-    { title: "White Tea", imgSrc: "https://cdn.shopify.com/s/files/1/0905/2012/files/teapoz_image_43.png", alt: "White Tea" },
+    {
+      title: "Tea Bags",
+      imgSrc:
+        "https://cdn.shopify.com/s/files/1/0905/2012/files/teapoz_image_45.png",
+      alt: "Tea Bags",
+    },
+    {
+      title: "Maté",
+      imgSrc:
+        "https://cdn.shopify.com/s/files/1/0905/2012/files/Mate_9.png?v=1685006070",
+      alt: "Maté",
+    },
+    {
+      title: "Matcha",
+      imgSrc:
+        "https://cdn.shopify.com/s/files/1/0905/2012/files/teapoz_image-54.png?v=1684921221",
+      alt: "Matcha",
+    },
+    {
+      title: "Oolong",
+      imgSrc:
+        "https://cdn.shopify.com/s/files/1/0905/2012/files/teapoz_image_49.png",
+      alt: "Oolong",
+    },
+    {
+      title: "Loose Tea",
+      imgSrc:
+        "https://cdn.shopify.com/s/files/1/0905/2012/files/teapoz_image_46.png",
+      alt: "Loose Tea",
+    },
+    {
+      title: "Green Tea",
+      imgSrc:
+        "https://cdn.shopify.com/s/files/1/0905/2012/files/teapoz_image_47.png",
+      alt: "Green Tea",
+    },
+    {
+      title: "Gift Sets",
+      imgSrc:
+        "https://cdn.shopify.com/s/files/1/0905/2012/files/teapoz_image_48.png",
+      alt: "Gift Sets",
+    },
+    {
+      title: "Teaware",
+      imgSrc:
+        "https://cdn.shopify.com/s/files/1/0905/2012/files/teaware.jpg?v=1685006326",
+      alt: "Teaware",
+    },
+    {
+      title: "Black Tea",
+      imgSrc:
+        "https://cdn.shopify.com/s/files/1/0905/2012/files/teapoz_image_44.png",
+      alt: "Black Tea",
+    },
+    {
+      title: "White Tea",
+      imgSrc:
+        "https://cdn.shopify.com/s/files/1/0905/2012/files/teapoz_image_43.png",
+      alt: "White Tea",
+    },
   ];
 
   return (
-    <section style={{ paddingTop: "20px" }}>
+    <section className="elementor-element elementor-element-e5ad149 elementor-section-boxed elementor-section-height-default elementor-section-height-inner-default elementor-section elementor-top-section">
       <Swiper
         draggable={true}
         grabCursor={true}
@@ -53,8 +109,12 @@ const HomeCat = () => {
                   cursor: "pointer",
                   transition: "transform 0.2s ease-in-out",
                 }}
-                onMouseEnter={(e) => (e.currentTarget.style.transform = "scale(1.05)")}
-                onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
+                onMouseEnter={(e) =>
+                  (e.currentTarget.style.transform = "scale(1.05)")
+                }
+                onMouseLeave={(e) =>
+                  (e.currentTarget.style.transform = "scale(1)")
+                }
               >
                 <img
                   style={{
